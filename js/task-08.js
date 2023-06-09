359e3ca4914dd0942c66fde582bfb04c538a6c80
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-06-09 17:14:34 +0300

    add 8th task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(e) {
+	e.preventDefault();
+	const email = e.target.elements.email.value;
+	const password = e.target.elements.password.value;
+	if (!email || !password) {
+		alert('Всі поля повинні бути заповнені!');
+		return;
+	}
+	const formData = {
+		email,
+		password,
+	};
+	console.log(formData);
+	form.reset();
+}
