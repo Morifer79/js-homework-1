63be1abe00be13deb27dbabdaf7ac69b68a0012f
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-06-09 21:01:29 +0300

    add 9th task

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const currentColor = document.querySelector('.color');
+
+button.addEventListener("click", () => {
+	currentColor.textContent = getRandomHexColor();
+	document.body.style.backgroundColor = getRandomHexColor();
+});
