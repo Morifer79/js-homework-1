275a5dd3163f8e09e7faa6ab2fc00666a5993b6b
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-06-10 20:12:42 +0300

    fix 2nd task

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,12 @@ const ingredients = [
 ];
 
 const ingredientsList = document.querySelector("#ingredients");
+const tempStorage = document.createDocumentFragment();
 
 const partsList = ingredients.map(el => {
 	const newEl = document.createElement("li");
 	newEl.classList.add("item");
 	newEl.textContent = el;
-	ingredientsList.append(newEl);
+	tempStorage.append(newEl);
 });
+ingredientsList.append(tempStorage);
